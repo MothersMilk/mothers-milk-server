0e8c7fc9a41d15b3ddfae8471deb98b9a757561d
Shane Moyo <smoyo@pdx.edu>
2017-12-09 11:08:02 -0800

    passing good model validation test

--- test/unit/location.test.js
+++ test/unit/location.test.js
@@ -5,7 +5,7 @@ const Location = require('../../lib/models/location');
 describe('Location model', () => {
 
     it('Should validate a good model', () => {
-        const loaction = new Location({
+        const location = new Location({
             name: 'Northwest Mothers Milk Bank',
             address: '417 SW 117th Ave, Portland, OR 97225',
             hours: '8AM–4:30PM'
