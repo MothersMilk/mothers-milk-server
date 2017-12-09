82dc0bab5516845acbc4c3c588ca3af440ae9260
Shane Moyo <smoyo@pdx.edu>
2017-12-09 11:38:38 -0800

    added mock loaction test data

--- test/e2e/locations.test.js
+++ test/e2e/locations.test.js
@@ -5,5 +5,28 @@ const assert = chai.assert;
 
 
 describe('location API', () => {
-    
-})
+    beforeEach(() => mongoose.connection.dropDatabase());
+
+    const testLocations = [
+        {
+            name: 'Milk Bank 1',
+            address: 'address 1 ',
+            hours: '8AM–4:30PM'
+        },
+        {
+            name: 'Milk Bank 2',
+            address: 'address 2 ',
+            hours: '8AM–4:30PM'
+        },
+        {
+            name: 'Milk Bank 3',
+            address: 'address 3',
+            hours: '8AM–4:30PM'
+        },
+    ];
+
+    it('Should save a location with an id', () => {
+        
+    })
+
+});
