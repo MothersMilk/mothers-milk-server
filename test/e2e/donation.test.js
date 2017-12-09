2c9a3c1fcd5c0d4774cb491824ecaa0a316211b7
Shane Moyo <smoyo@pdx.edu>
2017-12-09 13:39:42 -0800

    now saving a location before each test

--- test/e2e/donation.test.js
+++ test/e2e/donation.test.js
@@ -4,5 +4,19 @@ const request = require('./request');
 const assert = chai.assert;
 
 describ('donation API', () => {
-    
+    beforeEach(() => mongoose.connection.dropDatabase());
+
+    let savedLocation = null;
+    const location = {
+        name: 'Northwest Mothers Milk Bank',
+        address: '417 SW 117th Ave, Portland, OR 97225',
+        hours: '8AM–4:30PM'
+    };
+
+    beforeEach(()=> {
+        mongoose.connection.dropDatabase();
+        return request.post('/api/locations')
+            .send(location)
+            .then(({ body }) => savedLocation = body);
+    });
 })
