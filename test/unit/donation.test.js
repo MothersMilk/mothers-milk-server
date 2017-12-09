88b7cff475490984e8ae541bc2528fb001959353
Shane Moyo <smoyo@pdx.edu>
2017-12-09 12:58:55 -0800

    added failing validates a good model test for donations

--- test/unit/donation.test.js
+++ test/unit/donation.test.js
@@ -1,6 +1,31 @@
 const assert = require('chai').assert; 
 const Donantion = require('../../lib/models/donation');
+const request = require('../e2e/request');
 
 describe('Donation model', () =>  {
-    
+
+    //todo: save a donor before each test and add ref to test schema
+    let savedLocation = null;
+    const location = {
+        name: 'Northwest Mothers Milk Bank',
+        address: '417 SW 117th Ave, Portland, OR 97225',
+        hours: '8AM–4:30PM'
+    };
+
+    beforeEach(()=> {
+        mongoose.connection.dropDatabase();
+        return request.post('/api/locations')
+            .send(location)
+            .then(({ body }) => savedLocation = body);
+    });
+
+    it('Should validate a good model', () => {
+        const Donantion = new Donation({
+            amount: 6,
+            location: location._id,
+            eta: '4:30PM'
+        });
+        assert.equal(location.validateSync(), undefined);
+    });
+
 })
