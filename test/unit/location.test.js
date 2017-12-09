31693ac5d4b750dac152ba0c380363e87461894b
Shane Moyo <smoyo@pdx.edu>
2017-12-09 11:06:28 -0800

    added location schema test for a valid model

---
+++ test/unit/location.test.js
@@ -0,0 +1,19 @@
+const assert = require('chai').assert;
+const Location = require('../../lib/models/location');
+
+
+describe('Location model', () => {
+
+    it('Should validate a good model', () => {
+        const loaction = new Location({
+            name: 'Northwest Mothers Milk Bank',
+            address: '417 SW 117th Ave, Portland, OR 97225',
+            hours: '8AM–4:30PM'
+        });
+        assert.equal(location.validateSync(), undefined);
+    });
+
+   
+
+
+})
