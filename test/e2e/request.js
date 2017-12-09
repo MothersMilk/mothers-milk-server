16992a3f7a645e1a07c0e259963e533ecde3e0fa
Shane Moyo <smoyo@pdx.edu>
2017-12-09 11:27:13 -0800

    request.js file fixed

--- test/e2e/request.js
+++ test/e2e/request.js
@@ -3,3 +3,10 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const http = require('http');
 const app = require('../../lib/app');
+
+const server = http.createServer(app);
+const request = chai.request(server);
+
+after(() => server.close());
+
+module.exports = request; 
