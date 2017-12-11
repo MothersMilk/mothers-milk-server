2a1217b98804a0156928dd5b0d137397cbab7c80
Shane Moyo <smoyo@pdx.edu>
2017-12-11 06:33:58 -0800

    added email and roles to staff schema

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -1,3 +1,18 @@
 const request = require('./request');
 const assert = require('chai').assert;
+const mongoose = require('mongoose');
 const db = require('./db');
+
+describe('Auth API', () => {
+
+    beforeEach(() => mongoose.connection.dropDatabase());
+    
+    let token = null; 
+    beforeEach(()=>{
+        return request
+            .post('/api/auth/signup')
+            .send({
+                
+            })
+    })
+})
