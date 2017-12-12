947c7c4dbb863aa029c3ad0d640e17de753f28a4
Robin Miller <rmillerpdx@gmail.com>
2017-12-12 13:25:15 -0800

    add superAdmin hardcoding to server.js

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const http = require('http');
 const app = require('./lib/app');
 const connect = require('./lib/connect');
 
+const adminToken = require('./test/e2e/adminToken');
+
 connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mothersmilk');
 
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
+
+
+adminToken();
 
 server.listen(port, () => {
     console.log('server running on', server.address().port);
