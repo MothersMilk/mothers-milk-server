20700c19b97afbc626a075962cf212396600586a
Robin Miller <rmillerpdx@gmail.com>
2018-01-05 13:45:33 -0800

    move io.js out of utils folder

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./lib/app');
 const connect = require('./lib/connect');
-const io = require('./lib/utils/io');
+const io = require('./lib/io');
 
 
 connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mothersmilk');
