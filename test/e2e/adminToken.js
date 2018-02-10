a68f7684a2726173cdfaf775e499c6a6a09185c0
Robin Miller <rmillerpdx@gmail.com>
2018-02-09 16:28:14 -0800

    refactor admin token for test compatibility

--- test/e2e/adminToken.js
+++ test/e2e/adminToken.js
@@ -2,14 +2,15 @@ const User = require('../../lib/models/user');
 const tokenService = require('../../lib/utils/tokenService');
 
 
-const user = new User({
-    email: 'superadmin@admin.com',
-    name: 'super admin',
-    roles: ['admin']
-});
-user.generateHash('password');
 
 const adminToken = () => {
+    const user = new User({
+        email: 'superadmin@admin.com',
+        name: 'super admin',
+        roles: ['admin']
+    });
+    user.generateHash('password');
+    
     return user.save()
         .then(user => {
             return tokenService.sign(user);
